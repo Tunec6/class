9d049b5869126688f1dba9b52b323fa592daec79
Tunec <vadimstepanov04@gmail.com>
2023-09-20 02:58:06 +0500

    c

---
+++ c#/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Monitor{
+    string monitors;
+    string resolution;
+
+    public Monitor(string monitors, string resolution){
+        this.monitors = monitors;
+        this.resolution = resolution; 
+    }
+
+    public void Monitors(){
+        System.Console.WriteLine("Название монитора:" + monitors);
+        System.Console.WriteLine("Разрешение монитора:" + resolution);
+    }
+
+}
+
+public class Prog{
+    public static void Main(string[] args){
+        Monitor monitor1 = new Monitor("Asus rog", "1920x1080");
+        Monitor monitor2 = new Monitor("Samsung", "2460x1920");
+
+        monitor1.Monitors();
+        monitor2.Monitors();
+    } 
+}
